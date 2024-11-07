b8ae885cf3d31a7d79b2faeef65693115d108259
Tyuiu.BerezovskayaGO@outlook.com <Tyuiu.BerezovskayaGO@outlook.com>
2024-11-07 13:25:25 +0500

    Task1

---
+++ Tyuiu.BerezovskayaGO.Sprint4.Task1.V29.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.BerezovskayaGO.Sprint4.Task1.V29.Lib
+{
+    public class DataService : ISprint4Task1V29
+    {
+        public int Calculate(int[] array)
+        {
+            int sumArray = 0;
+
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    sumArray = sumArray + array[i];
+                }
+            }
+            return sumArray;
+        }
+    }
+}
